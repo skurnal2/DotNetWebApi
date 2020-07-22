9d90885df6a8ea28edf1ea503f5188c933c2ebdb
skurnal2 <skurnal2@studentmail.nait.ca>
2020-07-22 17:52:36 -0600

    POST works

---
+++ WebApiExerciseVintriTech/WebApiExerciseVintriTech/Controllers/BeerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiExerciseVintriTech.Helpers;
+
+namespace WebApiExerciseVintriTech.Controllers
+{
+    public class BeerController : ApiController
+    {
+        // POST api/beer/id
+        public HttpResponseMessage Post(int id, [FromBody] BeerRating rating)
+        {
+            var message = Request.CreateResponse(HttpStatusCode.Created, "It Works! You sent id " + id);
+            return message;
+        }
+    }
+}
---
+++ WebApiExerciseVintriTech/WebApiExerciseVintriTech/Helpers/BeerRating.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiExerciseVintriTech.Helpers
+{
+    public class BeerRating
+    {
+        public String Username { get; set; }
+        public double Rating { get; set; }
+        public String Comment { get; set; }
+    }
+}
