4974ec61dc071e14c1fcb3e25d563463ca22df82
skurnal2 <skurnal2@studentmail.nait.ca>
2020-07-22 21:58:56 -0600

    Task 1 halfway done - Now Requests work

--- WebApiExerciseVintriTech/WebApiExerciseVintriTech/Controllers/BeerController.cs
+++ WebApiExerciseVintriTech/WebApiExerciseVintriTech/Controllers/BeerController.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using WebApiExerciseVintriTech.Helpers;
+using WebApiExerciseVintriTech.Helpers.API;
 
 namespace WebApiExerciseVintriTech.Controllers
 {
@@ -13,7 +14,35 @@ namespace WebApiExerciseVintriTech.Controllers
         // POST api/beer/id
         public HttpResponseMessage Post(int id, [FromBody] BeerRating rating)
         {
-            var message = Request.CreateResponse(HttpStatusCode.Created, "It Works! You sent id " + id);
+            HttpResponseMessage message = null;
+            List<string> errors = new List<string>();
+
+            PunkApi punkObj = new PunkApi();
+            if (!punkObj.CheckIdExists(id))
+            {
+                errors.Add("ID does not exist");                
+            }
+
+            if (rating.Rating < 1 || rating.Rating > 5)
+            {
+                errors.Add("Not a valid rating. Should be between 1 and 5");
+            }
+
+            Dictionary<string, string[]> errorsDictionary = new Dictionary<string, string[]>();
+            errorsDictionary.Add("errors", errors.ToArray());
+
+            if (errors.Count > 0)
+            {
+                message = Request.CreateResponse(HttpStatusCode.BadRequest, errorsDictionary);
+            }
+            else
+            {
+                message = Request.CreateResponse(HttpStatusCode.OK , rating);
+            }
+
+            
+
+
             return message;
         }
     }
---
+++ WebApiExerciseVintriTech/WebApiExerciseVintriTech/Helpers/API/PunkApi.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace WebApiExerciseVintriTech.Helpers.API
+{
+    public class PunkApi
+    {
+        string root_url = "https://api.punkapi.com/v2/beers";
+
+        public bool CheckIdExists(int id) {
+
+            //Concatenating id sent in method with the API url
+            string customUrl = root_url + "/" + id;
+            WebRequest requestObjGet = WebRequest.Create(customUrl);
+            requestObjGet.Method = "GET";
+            HttpWebResponse responseObjGet = null;
+
+            try
+            {
+                //Getting the response code
+                responseObjGet = requestObjGet.GetResponse() as HttpWebResponse;
+
+                //If Code returned is 200 OK, then return true, otherwise return false
+                if (responseObjGet.StatusCode == HttpStatusCode.OK)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (WebException ex)
+            {
+                //If in some case, error occurs, or 404 returned - return false
+                responseObjGet = ex.Response as HttpWebResponse;
+                return false;
+            }
+
+           
+        }
+    }
+}
